85309d680d9a4ba053ca35d1a58fa016421b3bfc
septian <septianibnyohan@gmail.com>
2022-12-21 05:44:38 +0700

    first commit

---
+++ content-script/src/main.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './main.css'
+import App from './App'
+import ButtonToggle from './ButtonToggle';
+
+// const body = document.querySelector('body')
+
+// const app = document.createElement('div')
+
+// app.id = 'root'
+
+// // Make sure the element that you want to mount the app to has loaded. You can
+// // also use `append` or insert the app using another method:
+// // https://developer.mozilla.org/en-US/docs/Web/API/Element#methods
+// //
+// // Also control when the content script is injected from the manifest.json:
+// // https://developer.chrome.com/docs/extensions/mv3/content_scripts/#run_time
+// if (body) {
+//   body.prepend(app)
+// }
+
+const E = document.createElement("div");
+E.id = "root";
+E.classList.add("transform");
+E.classList.add("ready");
+document.body.appendChild(E)
+
+const container = document.getElementById('root');
+const root = createRoot(container!);
+
+root.render(
+  <React.StrictMode>
+    <div className=''>
+      <ButtonToggle />
+    </div>
+    <App />
+  </React.StrictMode>
+)
